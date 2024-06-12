c55af29c299031d279ccf0fd15ab0c6b635e5c30
kumar-sarwan67 <sarwanmalhi67@gmail.com>
2024-06-12 14:42:22 +0000

    datatype

---
+++ 01batch/varible.js
@@ -0,0 +1,7 @@
+const accountId = 123533
+let accountEmail =  "sarwankumar@gmail"
+var accountPassword = "153445"
+accountCity = "Umerkot"
+console.log(accountCity);
+let accountstate;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
